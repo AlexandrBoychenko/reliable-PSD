ac3f68754a9bf52b5aa7d15de689631bbffdeb2f
AlexandrBoychenko <alex19wave>
2019-02-21 12:05:17 +0200

    Media tag has been added. Responsive styles are changed

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,9 +1,13 @@
-$(function(){
+$(() => {
     hideAnotherImages();
-    $('.slider-wrapper img').click(function(){
-        $('.slider-wrapper :first-child').fadeOut(()=> {$('.slider-wrapper img:gt(0)').hide();}).next('img').fadeIn().end().appendTo('.slider-wrapper');
-        hideAnotherImages()
-    });
+    $('.slider-wrapper img').click(() => {
+        $('.slider-wrapper :first-child')
+            .fadeOut(()=> {
+            $('.slider-wrapper img:gt(0)').hide();
+            })
+            .next('img').fadeIn().end().appendTo('.slider-wrapper');
+            hideAnotherImages()
+        });
 });
 
 function hideAnotherImages() {
